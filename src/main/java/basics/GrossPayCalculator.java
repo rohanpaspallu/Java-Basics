dbc99ac38f563c2c08e3dc1527dca910629583f9
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-07-22 14:11:16 -0400

    basic implementation of pay calculator

--- src/main/java/basics/GrossPayCalculator.java
+++ src/main/java/basics/GrossPayCalculator.java
@@ -1,4 +1,18 @@
 package basics;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
+    public static void main(String[] args) {
+        System.out.println("How many hours did you work ?");
+//        Scanner is used to read from the command line
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+        System.out.println("what is your hourly pay rate ?");
+        double rate = scanner.nextDouble();
+        scanner.close();
+
+        double payRate = hours*rate;
+        System.out.println("Gross pay : "+payRate);
+    }
 }
