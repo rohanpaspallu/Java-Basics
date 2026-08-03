ba088334031b568ebdca5f4df11b5824fa803dfa
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-08-03 18:00:40 -0400

    basic switch

---
+++ src/main/java/decision_structure/GradeMessage.java
@@ -0,0 +1,37 @@
+package decision_structure;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+    public static void main(String[] args) {
+        System.out.println("Enter your letter grade : ");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next().toUpperCase();
+        scanner.close();
+
+        String message;
+
+        switch (grade){
+            case "A":
+                message = "Excellent job";
+                break;
+
+            case "B":
+                message="Great job";
+                break;
+            case "C":
+                message = "Good job";
+                break;
+            case "D":
+                message = "Work a bit harder";
+                break;
+            case "F":
+                message = "Uh oh!";
+                break;
+            default:
+                message = "Not a vaild grade";
+                break;
+        }
+        System.out.println(message);
+    }
+}
