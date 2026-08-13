e7e5b59cf05112c934aefb1d458c67a9077384fa
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-08-13 14:49:04 -0400

    reverse a string

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 public class TextProcessor {
     public static void main(String[] args) {
         countWords("I love java");
+        reverseString("Hello World");
+    }
+
+    public static void reverseString(String text) {
+        String newStr;
+        for(int i= text.length()-1; i>=0; i--){
+            System.out.print(text.charAt(i));
+        }
     }
 
     public static void countWords(String text) {
