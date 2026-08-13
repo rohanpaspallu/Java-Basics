33eaa79c9bdbe5c3c74162f4aa164d83822d1710
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-08-13 15:04:10 -0400

    example of string builder

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -4,8 +4,20 @@ import java.util.Arrays;
 
 public class TextProcessor {
     public static void main(String[] args) {
-        countWords("I love java");
-        reverseString("Hello World");
+//        countWords("I love java");
+//        reverseString("Hello World");
+        addSpaces("HeyWorld!It'sMeRohan");
+    }
+
+    public static void addSpaces(String s) {
+        var modifiedText = new StringBuilder(s);
+        for(int i=0; i<modifiedText.length(); i++){
+            if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
     }
 
     public static void reverseString(String text) {
