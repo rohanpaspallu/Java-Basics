eded300f6bc372c27d769a56e19d7aae92ca7910
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-08-17 15:42:11 -0400

    Inheritance along with super constructor

---
+++ src/main/java/inheritance/InheritanceTester.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class InheritanceTester {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Employee employee1 = new Employee();
+
+    }
+}
