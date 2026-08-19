4a7f42b4a0585272857410c86167914323c708ff
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-08-19 15:49:32 -0400

    Lists

--- src/main/java/data_structures/CollectionsDemo.java
+++ src/main/java/data_structures/CollectionsDemo.java
@@ -1,11 +1,33 @@
 package data_structures;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
-        setDemo();
+//        setDemo();
+        listDemo();
+    }
+
+    public static void listDemo() {
+        List fruits = new ArrayList();
+
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("banana");
+        fruits.add("orange");
+
+        fruits.set(2, "grapes");
+        fruits.add("lemon");
+//        fruits.remove("lemon");
+        System.out.println("Arraylist of fruits : "+ fruits);
+        System.out.println("Item at index 2 is : "+ fruits.get(2));
+        System.out.println("Index of orange is : "+ fruits.indexOf("orange"));
+        System.out.println("Get last index of Lemon : "+ fruits.lastIndexOf("lemon"));
+        List moreFruits = List.of("cherry", "plum"); //Immutable
+
     }
 
     public static void setDemo() {
