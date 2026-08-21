fcf79fba19f7857bd1fe847fabcfd96b963e14ee
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-08-21 14:29:09 -0400

    usage of finally and try with parameter

--- src/main/java/exceptions/TryWithResources.java
+++ src/main/java/exceptions/TryWithResources.java
@@ -2,16 +2,19 @@ package exceptions;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TryWithResources {
 
     public static void main(String[] args) {
-        File file = new File("files/numbers.txt");
-        try(Scanner fileReader = new Scanner(file)){
+        File inputFile = new File("files/numbers.txt");
+        File outputFile = new File("file/output.txt");
+        try(Scanner fileReader = new Scanner(file); PrintWriter fileWriter = new PrintWriter(outputFile)){
             while (fileReader.hasNext()){
-                System.out.println(fileReader.nextDouble());
+                fileWriter.println(fileReader.nextDouble());
+//                System.out.println(fileReader.nextDouble());
             }
         }
 
